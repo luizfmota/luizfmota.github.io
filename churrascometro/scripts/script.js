d572316507711bad74dd786bd7306299c9fe1f76
Luiz Mota <67341991+luizfmota@users.noreply.github.com>
2021-01-05 02:01:37 -0300

    Update script.js

--- churrascometro/scripts/script.js
+++ churrascometro/scripts/script.js
@@ -17,15 +17,40 @@ function calcular() {
     let criancas = inputCriancas.value;
     let duracao = inputDuracao.value;
 
-    let qtdTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas);
-    let qtdTotalLinguica = linguicaPP(duracao) * adultos + (linguicaPP(duracao) / 2 * criancas);
-    let qtdTotalCerveja = cervejaPP(duracao) * adultos;
-    let qtdTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
-    
-    resultado.innerHTML = `<p><img class="f-left" src="./images/bife.png"/>  ${qtdTotalCarne / 1000} Kg de Carne  <img class="f-right" src="./images/bife.png"/></p>`;
-    resultado.innerHTML += `<p><img class="f-left" src="./images/linguica.png"/>  ${qtdTotalLinguica / 1000} Kg de Linguiça  <img class="f-right" src="./images/linguica.png"/></p>`;
-    resultado.innerHTML += `<p><img class="f-left" src="./images/beer.png"/>  ${Math.ceil(qtdTotalCerveja / 355)} Latas de Cerveja  <img class="f-right" src="./images/beer.png"/></p>`;
-    resultado.innerHTML += `<p><img class="f-left" src="./images/refrigerante.png"/>  ${Math.ceil(qtdTotalBebidas / 2000)} Pet's de 2l de Bebidas  <img class="f-right" src="./images/refrigerante.png"/></p>`;
+    if (adultos == "" || criancas == "" || duracao == "") {
+        resultado.innerHTML = `<h3>Não deu :(</h3>
+                                    <p>Está faltando algum valor!</p>`;
+        return false;
+    } else {
+        let qtdTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas);
+        let qtdTotalLinguica = linguicaPP(duracao) * adultos + (linguicaPP(duracao) / 2 * criancas);
+        let qtdTotalCerveja = cervejaPP(duracao) * adultos;
+        let qtdTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
+        
+        resultado.innerHTML = `<p>
+                                    <img class="f-left" src="./images/bife.png"/>  
+                                        ${qtdTotalCarne / 1000} Kg de Carne  
+                                    <img class="f-right" src="./images/bife.png"/>
+                                </p>`;
+
+        resultado.innerHTML += `<p>
+                                    <img class="f-left" src="./images/linguica.png"/>  
+                                        ${qtdTotalLinguica / 1000} Kg de Linguiça  
+                                    <img class="f-right" src="./images/linguica.png"/>
+                                </p>`;
+
+        resultado.innerHTML += `<p>
+                                    <img class="f-left" src="./images/beer.png"/>  
+                                        ${Math.ceil(qtdTotalCerveja / 355)} latas de Cerveja  
+                                    <img class="f-right" src="./images/beer.png"/>
+                                </p>`;
+
+        resultado.innerHTML += `<p>
+                                    <img class="f-left" src="./images/refrigerante.png"/>  
+                                        ${Math.ceil(qtdTotalBebidas / 2000)} Pet's de 2l de Bebidas  
+                                    <img class="f-right" src="./images/refrigerante.png"/>
+                                </p>`;
+    }
 }
 
 function carnePP(duracao) {
